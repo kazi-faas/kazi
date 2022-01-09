2b2406576eafee5091f8e714b7d9b1610544ae25
Peter Mbanugo <peter@foobar.agency>
2022-01-09 17:17:48 +0100

    remove spacing

--- source/meow-util.ts
+++ source/meow-util.ts
@@ -1,16 +1,15 @@
 export const helpText = `
 Commands
-	$ kazi create: Scaffold a new project
-	$ kazi list: List the functions deployed using kazi
-	$ kazi deploy: Deploy a function (only works if you're inside the function's directory)
+  $ kazi create: Scaffold a new project
+  $ kazi list: List the functions deployed using kazi
+  $ kazi deploy: Deploy a function (only works if you're inside the function's directory)
 
 Usage
   $ kazi create <Function_Name>
 
 Options
-	--registry  Your registry namespace
+  --registry  Your registry namespace
 
 Examples
   $ kazi create hello --name=docker.io/pmbanugo
-
 `;
